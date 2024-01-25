378e7418f00c13a2b273f965c6641da4d6c239cd
Martin Lin <140759363+LinuxSecUp@users.noreply.github.com>
2024-01-25 22:48:22 +0200

    Add files via upload

---
+++ ScanPort/fileScan.h
@@ -0,0 +1,17 @@
+#pragma once
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+namespace FileScan {
+
+	auto FileOption() {
+		Sleep(500);
+		cout << "Hello to programm Linux: " << endl;
+		Sleep(1200);
+		cout << "Enter language" << endl;
+		
+	}
+}
+
